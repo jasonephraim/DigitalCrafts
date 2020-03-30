79971b0efe28701b16b992084ccf91b3335c8472
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-03-30 14:35:42 -0500

    moved file dupes back and added moving script

---
+++ Projects/Assignment-CLI/Version-1/move-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv /Version-2/index-2.html ../Version-1
+mv /Version-2/services-2.html ../Version-1
---
+++ Projects/Assignment-CLI/move-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mv index-2.html ../Version-2
+mv services-2.html ../Version2
