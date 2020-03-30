27bbd500cb38b92dece303f5e422ac75cb527f24
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-03-30 14:47:41 -0500

    corrected script to generate all files, folders and move dupes

--- Projects/Assignment-CLI/Version-1/move-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mv /Version-2/index-2.html ../Version-1
-mv /Version-2/services-2.html ../Version-1
--- Projects/Assignment-CLI/move-script.sh
+++ Projects/Assignment-CLI/move-script.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+mkdir Version-1
+mkdir Version-2
+cd Version-1
+touch index.html
+touch contact-us.html
+touch services.html
+touch about-us.html
+touch index-2.html
+touch services-2.html
 mv index-2.html ../Version-2
-mv services-2.html ../Version2
+mv services-2.html ../Version-2
+echo "All Done!"
+cd ..
