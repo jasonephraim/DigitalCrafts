d82cd4de3154bb54fb707c2703bb7494319d0022
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2020-04-07 17:21:24 -0500

    Update palindrome.py
    extracted palindrome into Palindrome class. refactored logic into methods set_query_string, is_palindrome, show_result. defined init() to run the application.

--- Projects/Python/Week 1/palindrome.py
+++ Projects/Python/Week 1/palindrome.py
@@ -1,9 +1,35 @@
-def palindrome(word = str.lower(input("Please enter a word: "))):
-    word2 =''.join(reversed(word))
-    if word == word2:
-        print(word, "is a palindrome!")
-    else:
-        print(word, "is not a palindrome")
-palindrome()
 
-# need to account for special characters and spaces 
+class Palindrome:
+    def __init__(self):
+        return
+    def set_query_string(self):
+        self.query_string = str.lower(input("Please enter a word: "))
+
+    def is_palindrome(self):
+        if ''.join(reversed(self.query_string)) == self.query_string:
+            return True
+        else:
+            return False
+
+    def show_result(self, boolean):
+        if boolean:
+            print(self.query_string, "is a palindrome!")
+        else:
+            print(self.query_string, "is not a palindrome")
+
+
+    
+def init():
+    self = Palindrome()
+    self.active = True
+    def is_active():
+        if str.lower(input("Do you wish to continue? (Y/N) ")) == 'n':
+            return False
+        else:
+            return True
+    while self.active:
+        self.set_query_string()
+        self.show_result(self.is_palindrome())
+        self.active = is_active()
+
+init()
